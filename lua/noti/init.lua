98c83004acc49d3eb0f9639225fb11ac31134440
Pavel <work.belyavsky.p.a@gmail.com>
2024-12-22 20:18:46 +0300

    feat: add support to use Comment.nvim

--- lua/noti/init.lua
+++ lua/noti/init.lua
@@ -7,17 +7,26 @@ function M.setup()
     }
   }
 
+  local set_comment, ft = pcall(require, "Comment.ft")
+  if set_comment then
+    ft.set("noti", {"//%s", "/*%s*/"})
+  end
+
   local noti_group = vim.api.nvim_create_augroup("Noti", {})
   vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     group = noti_group,
     pattern = { "*.noti" },
     desc = "Creates the specific commentstring for .noti filetype",
     callback = function(data)
-      vim.api.nvim_set_option_value("commentstring", "// %s", { buf = data.buf })
+      if not set_comment then
+        vim.api.nvim_set_option_value("commentstring", "//%s", { buf = data.buf })
+      end
+
       vim.treesitter.start(data.buf, "noti")
     end,
   })
 
+  ---@class ParserInfo[]
   local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
   parser_config.noti = {
     install_info = {
